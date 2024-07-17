e7f15017a4493c4f5b723f9e4a5234af5f1a104b
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-17 10:17:18 +0530

    working action buttons

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -5,7 +5,8 @@ import { getInsertionSortAnimations } from "../SortingAlgorithms/InsertionSort";
 import { getBubbleSortAnimations } from "../SortingAlgorithms/BubbleSort";
 import { getHeapSortAnimations } from "../SortingAlgorithms/HeapSort";
 import { getQuickSortAnimations } from "../SortingAlgorithms/QuickSort";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaPlay, FaPause } from "react-icons/fa";
+import { MdShuffle } from "react-icons/md";
 const ARRAY_SIZE = 150;
 
 export class SortingVisualizer extends React.Component {
@@ -16,6 +17,7 @@ export class SortingVisualizer extends React.Component {
             array: [],
             selectedSort: 'Select a sort from menu',
             showMenu: false,
+            playBtn: true
         };
     }
 
@@ -92,19 +94,19 @@ export class SortingVisualizer extends React.Component {
 
     handleSort() {
         switch (this.state.selectedSort) {
-            case 'mergeSort':
+            case 'Merge Sort':
                 this.mergeSort();
                 break;
-            case 'quickSort':
+            case 'Quick Sort':
                 this.quickSort();
                 break;
-            case 'heapSort':
+            case 'Heap Sort':
                 this.heapSort();
                 break;
-            case 'insertionSort':
+            case 'Insertion Sort':
                 this.insertionSort();
                 break;
-            case 'bubbleSort':
+            case 'Bubble Sort':
                 this.bubbleSort();
                 break;
             default:
@@ -114,6 +116,10 @@ export class SortingVisualizer extends React.Component {
 
     toggleMenu = () => {
         this.setState(prevState => ({ showMenu: !prevState.showMenu }));
+    }
+
+    togglePlayBtn = () => {
+        this.setState(prevState => ({playBtn: !prevState.playBtn }));
     }
 
     render() {
@@ -139,34 +145,36 @@ export class SortingVisualizer extends React.Component {
                     </button>
                     <div className='sort-options' onChange={(event) => this.handleSortChange(event)}>
                         <div className='sort-option'>
-                            <input type="radio" id="mergeSort" name="sort" value="mergeSort" checked={selectedSort === 'mergeSort'} />
+                            <input type="radio" id="mergeSort" name="sort" value="Merge Sort" checked={selectedSort === 'Merge Sort'} />
                             <label htmlFor="mergeSort">Merge Sort</label>
                         </div>
                         <div className='sort-option'>
-                            <input type="radio" id="quickSort" name="sort" value="quickSort" checked={selectedSort === 'quickSort'} />
+                            <input type="radio" id="quickSort" name="sort" value="Quick Sort" checked={selectedSort === 'Quick Sort'} />
                             <label htmlFor="quickSort">Quick Sort</label>
                         </div>
                         <div className='sort-option'>
-                            <input type="radio" id="heapSort" name="sort" value="heapSort" checked={selectedSort === 'heapSort'} />
+                            <input type="radio" id="heapSort" name="sort" value="Heap Sort" checked={selectedSort === 'Heap Sort'} />
                             <label htmlFor="heapSort">Heap Sort</label>
                         </div>
                         <div className='sort-option'>
-                            <input type="radio" id="insertionSort" name="sort" value="insertionSort" checked={selectedSort === 'insertionSort'} />
+                            <input type="radio" id="insertionSort" name="sort" value="Insertion Sort" checked={selectedSort === 'Insertion Sort'} />
                             <label htmlFor="insertionSort">Insertion Sort</label>
                         </div>
                         <div className='sort-option'>
-                            <input type="radio" id="bubbleSort" name="sort" value="bubbleSort" checked={selectedSort === 'bubbleSort'} />
+                            <input type="radio" id="bubbleSort" name="sort" value="Bubble Sort" checked={selectedSort === 'Bubble Sort'} />
                             <label htmlFor="bubbleSort">Bubble Sort</label>
                         </div>
                     </div>
                 </div> }
-                
-                <div className='selected-sort-display'>
-                    {selectedSort}
-                </div>
-                <div className='action-buttons'>
-                    <button className='generate-new-bars-btn' onClick={() => this.resetArray()}>Generate New Bars</button>
-                    <button className='sort-btn' onClick={() => this.handleSort()} disabled={!selectedSort}>Sort</button>
+
+                <div className="display-container">
+                    <div className='selected-sort-display'>
+                        {selectedSort}
+                    </div>
+                    <div className='action-btns'>
+                        <button className='generate-new-bars-btn' onClick={() => this.resetArray()}> <MdShuffle className="shuffle-icon" /> <h2>Shuffle</h2> </button>
+                        <button className='sort-play-btn' onClick={() => this.handleSort()} disabled={!selectedSort}> <FaPlay className="play-icon"/> <h2>Sort</h2> </button>
+                    </div>
                 </div>
 
                 <div className='array-bar-container'>
